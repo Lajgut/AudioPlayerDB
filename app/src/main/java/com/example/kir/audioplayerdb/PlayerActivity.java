276f34be89e3942759316fd3c4e8fb706d06ab44
Kir <Kirbitkov@mail.ru>
2016-09-26 12:20:23 +0300

    exception?

--- app/src/main/java/com/example/kir/audioplayerdb/PlayerActivity.java
+++ app/src/main/java/com/example/kir/audioplayerdb/PlayerActivity.java
@@ -7,16 +7,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 import java.io.File;
 import java.util.ArrayList;
 
 public class PlayerActivity extends AppCompatActivity implements View.OnClickListener {
 
-    MediaPlayer mPlayer;
+    static  MediaPlayer mPlayer;
     ArrayList<File> mTracks;
     int position;
+    Uri uri;
     Button btnPlayPause, btnStop, btnNext, btnPrev;
+    SeekBar sb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +30,28 @@ public class PlayerActivity extends AppCompatActivity implements View.OnClickLis
         btnStop = (Button) findViewById(R.id.btnStop);
         btnNext = (Button) findViewById(R.id.btnNext);
         btnPrev = (Button) findViewById(R.id.btnPrev);
+        sb = (SeekBar) findViewById(R.id.sb);
 
         btnPlayPause.setOnClickListener(this);
         btnStop.setOnClickListener(this);
         btnNext.setOnClickListener(this);
         btnPrev.setOnClickListener(this);
 
-        btnPlayPause.setText(R.string.pause);
+        btnPlayPause.setText("||");
+        btnNext.setText(">|");
+        btnPrev.setText("|<");
+
+        if (mPlayer!=null){             //остановить проигрывание трека при переходе к следующему
+            mPlayer.stop();
+            mPlayer.release();
+        }
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         ArrayList<File> mTracks = (ArrayList) bundle.getParcelableArrayList("tracks");
         position = bundle.getInt("position",0);
 
-        Uri uri = Uri.parse(mTracks.get(position).toString());
+        uri = Uri.parse(mTracks.get(position).toString());
         mPlayer = MediaPlayer.create(getApplicationContext(),uri);
         mPlayer.start();
     }
@@ -51,17 +62,16 @@ public class PlayerActivity extends AppCompatActivity implements View.OnClickLis
             switch (id){
                 case R.id.btnPlayPause:
                     if (mPlayer.isPlaying()){
-                        btnPlayPause.setText(R.string.play);
+                        btnPlayPause.setText(">");
                         mPlayer.pause();
-
-                    } else
+                    } else {
+                        btnPlayPause.setText("||");
                         mPlayer.start();
-                        btnPlayPause.setText(R.string.pause);
+                    }
                     break;
 
                 case R.id.btnStop:
                     if (mPlayer.isPlaying()) {
-                        btnPlayPause.setText(R.string.play);
                     }
                     mPlayer.stop();
                     mPlayer.release();
@@ -71,13 +81,19 @@ public class PlayerActivity extends AppCompatActivity implements View.OnClickLis
                 case R.id.btnNext:
                     mPlayer.stop();
                     mPlayer.release();
-                    Uri uri = Uri.parse(mTracks.get(position+1).toString());
+                    position = (position+1)%mTracks.size();
+                    uri = Uri.parse(mTracks.get(position).toString());
                     mPlayer = MediaPlayer.create(getApplicationContext(),uri);
                     mPlayer.start();
                     break;
 
                 case R.id.btnPrev:
-
+                    mPlayer.stop();
+                    mPlayer.release();
+                    position = (position - 1 < 0)? mTracks.size()-1: position - 1; // если позиция больше нуля, то возвращает 1е значение, иначе 2е (типо отучаюсь говнокодить)
+                    uri = Uri.parse(mTracks.get(position).toString());
+                    mPlayer = MediaPlayer.create(getApplicationContext(),uri);
+                    mPlayer.start();
                     break;
 
             }
